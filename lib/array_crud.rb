9e8370c36100594ee45933b4bdf7610f97031698
Chana Kupetz <chanakupetz@gmail.com>
2020-07-21 19:05:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   ["avi", "chana", "ori", "niv"]
 end
 
-def add_element_to_end_of_array(array)
-  p array.push(element)
+def add_element_to_end_of_array(array, element)
+  (array, element).push(element)
 end
 
 def add_element_to_start_of_array(array, element)
